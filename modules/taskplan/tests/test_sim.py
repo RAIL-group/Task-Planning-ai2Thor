78565fc0005425ce3f19b1980adc9143a7ecdd2a
raihan <arnob2601@yahoo.com>
2024-07-14 00:07:25 -0400

    added graph on grid test

--- modules/taskplan/tests/test_sim.py
+++ modules/taskplan/tests/test_sim.py
@@ -1,15 +1,15 @@
 import os
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 
 import taskplan
+import taskplan.plotting
 
 
 def get_args():
     create_dir()
     args = lambda key: None
-    args.current_seed = 0
+    args.current_seed = 1
     args.resolution = 0.05
     return args
 
@@ -30,7 +30,7 @@ def test_reachable_grid():
     args = get_args()
     save_file = '/data/test_logs/grid-scene-' \
         + str(args.current_seed) + '.png'
-    random.seed(args.current_seed)
+
     # # Get data for a send and extract initial object states
     thor_data = taskplan.utilities.ai2thor_helper. \
         ThorInterface(args=args, preprocess=True)
@@ -45,6 +45,38 @@ def test_reachable_grid():
 
     plt.subplot(122)
     plt.imshow(top_down_frame)
+    plt.savefig(save_file, dpi=1200)
+
+    assert 0 == 0
+
+
+def test_graph_on_grid():
+    ''' This test plots occupancy grid and the original top-view image of the same procTHOR map.
+    '''
+    args = get_args()
+    save_file = '/data/test_logs/graph-on-grid-' \
+        + str(args.current_seed) + '.png'
+
+    # Get thor data for a send and extract initial object states
+    thor_data = taskplan.utilities.ai2thor_helper. \
+        ThorInterface(args=args)
+    # Get the occupancy grid from proc_data
+    grid = thor_data.occupancy_grid
+    init_robot_pose = thor_data.get_robot_pose()
+
+    # Get the whole graph from ProcTHOR data
+    whole_graph = thor_data.get_graph()
+
+    plt.subplot(121)
+    taskplan.plotting.plot_graph_on_grid(grid, whole_graph)
+    x, y = init_robot_pose
+    plt.text(x, y, '+', color='red', size=6, rotation=45)
+
+    # plot top-down view from simulator
+    plt.subplot(122)
+    top_down_frame = thor_data.get_top_down_frame()
+    plt.imshow(top_down_frame)
+
     plt.savefig(save_file, dpi=1200)
 
     assert 0 == 0
