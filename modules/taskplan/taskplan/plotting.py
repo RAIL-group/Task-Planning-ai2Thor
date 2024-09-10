f06993c6c73f40c7cf5aedbc8d4c471b53d397e7
Raihan Islam Arnob <arnob2601@yahoo.com>
2024-09-09 22:04:27 -0400

    included plan plotting function

--- modules/taskplan/taskplan/plotting.py
+++ modules/taskplan/taskplan/plotting.py
@@ -97,3 +97,23 @@ def simulate_plan(trajectory, thor_data, args):
         writer.grab_frame()
     writer.finish()
     print(f'Seed[{args.current_seed}]: Video Saved!')
+
+
+def plot_plan(plan):
+    # Add a text block
+    textstr = ''
+    for p in plan:
+        textstr += str(p) + '\n'
+    # textstr = 'This is a text block.\nYou can add multiple lines of text.'
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+    # Place a text box in upper left in axes coords
+    plt.text(0, 1, textstr, transform=plt.gca().transAxes, fontsize=5,
+             verticalalignment='top', bbox=props)
+    plt.box(False)
+    # Hide x and y ticks
+    plt.xticks([])
+    plt.yticks([])
+
+    # Add labels and title
+    plt.title('Plan progression', fontsize=6)
