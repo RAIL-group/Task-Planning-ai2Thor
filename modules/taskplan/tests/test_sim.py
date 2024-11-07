5c94869bd1ba000bc0de09a25057e6dcf68e29b4
Raihan Islam Arnob <arnob2601@yahoo.com>
2024-11-06 23:48:53 -0500

    added test to plot custom trajectory

--- modules/taskplan/tests/test_sim.py
+++ modules/taskplan/tests/test_sim.py
@@ -80,3 +80,70 @@ def test_graph_on_grid():
     plt.savefig(save_file, dpi=1200)
 
     assert 0 == 0
+
+
+def test_plot_trajectory():
+    args = get_args()
+    args.current_seed = 7004
+    save_file = '/data/test_logs/trajectory-' \
+        + str(args.current_seed) + '.png'
+
+    # Get thor data for a send and extract initial object states
+    thor_data = taskplan.utilities.ai2thor_helper. \
+        ThorInterface(args=args)
+    # Get the occupancy grid from proc_data
+    grid = thor_data.occupancy_grid
+    init_robot_pose = thor_data.get_robot_pose()
+
+    # Get the whole graph from ProcTHOR data
+    whole_graph = thor_data.get_graph()
+
+    # Initialize the PartialMap with whole graph
+    partial_map = taskplan.core.PartialMap(whole_graph, grid, distinct=True)
+
+    # print(whole_graph['node_names'])
+    # print(whole_graph['node_coords'])
+
+    # # unaudited trajectory
+    # cost_str = 'Requires auditing'
+    # robot_poses = [init_robot_pose, (106, 101), (182, 112), (196, 144), (176, 184)]
+    # # dining table (106, 101)
+    # # bed (182, 112)
+    # # arm-chair (196, 144)
+    # # fridge (176, 184)
+
+    # audited trajectory
+    cost_str = 'With auditing'
+    robot_poses = [init_robot_pose, (176, 184)]
+
+    distance, trajectory = taskplan.core.compute_path_cost(partial_map.grid, robot_poses)
+
+    # 1 plot the grid with trajectory viridis color
+    plt.figure(figsize=(10, 6))
+    plt.subplot(121)
+    plotting_grid = taskplan.plotting.make_blank_grid(
+        np.transpose(grid)
+    )
+    plt.imshow(plotting_grid)
+    plt.title(f"{cost_str} Cost: {distance:0.3f}", fontsize=6)
+    plt.xticks(fontsize=5)
+    plt.yticks(fontsize=5)
+
+    viridis_cmap = plt.get_cmap('viridis')
+
+    colors = np.linspace(0, 1, len(trajectory[0]))
+    line_colors = viridis_cmap(colors)
+
+    for idx, x in enumerate(trajectory[0]):
+        y = trajectory[1][idx]
+        plt.plot(x, y, color=line_colors[idx], marker='.', markersize=3)
+
+    plt.subplot(122)
+    top_down_frame = thor_data.get_top_down_frame()
+    plt.imshow(top_down_frame)
+
+    # Hide box and ticks
+    plt.box(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig(f'{save_file}', dpi=1200)
